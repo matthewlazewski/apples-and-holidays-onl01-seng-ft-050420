a75aae50f0a4129fba99a6a73f44f529c6beada0
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-08 02:45:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,11 +76,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.map do |season, holiday|
+    holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
-      return holiday if item.include?("BBQ")
+      holiday if item.include?("BBQ")
     end 
-  end
+  end.flatten.compact
 end
 
 
