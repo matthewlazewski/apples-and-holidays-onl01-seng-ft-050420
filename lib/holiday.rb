cbaa1fc706a791b417b971c839486249580c8805
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-08 02:33:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,6 +34,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  
   if season == :spring
       holiday.each do |day, array|
         if array.is_a? Array 
